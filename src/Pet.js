6f24743c106ce8b6c45894fb919450e0b99eeb5d
Dusty211 <kylebenz@hotmail.com>
2019-03-04 17:49:51 -0500

    super cool methods, man

--- src/Pet.js
+++ src/Pet.js
@@ -6,8 +6,32 @@ class Pet {
     this.happiness = petObj.happiness
     this.image = petObj.image
     this.active_status = petObj.active_status
+    this.characteristics = petObj.characteristics
     Pet.all.push(this)
   }
+
+  happinessUp(characteristicName) {
+    const char = this.characteristics.find(char => {
+      return char.name === characteristicName
+    })
+    this.happiness += char.incr
+    if (this.happiness > 100) {
+      this.happiness = 100
+    }
+    console.log(this.happiness)
+  }
+
+  happinessDown(characteristicName) {
+    const char = this.characteristics.find(char => {
+      return char.name === characteristicName
+    })
+    this.happiness -= char.decr
+    if (this.happiness < 0) {
+      this.happiness = 0
+    }
+    console.log(this.happiness)
+  }
+
 }
 
 Pet.all = []
