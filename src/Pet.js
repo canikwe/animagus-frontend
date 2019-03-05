f3ab042b40046db6d5923eca814f036d76481378
hai-nguyen1112 <hainguyen871112@gmail.com>
2019-03-05 15:54:19 -0500

    add css to pet form

--- src/Pet.js
+++ src/Pet.js
@@ -47,25 +47,25 @@ class Pet {
 
 
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   pName(){
@@ -76,44 +76,44 @@ class Pet {
     h4.innerText = this.name
     return h4
   }
-  
+
   pStats(){
     //create p element to display pet stats
     const p = document.createElement('p')
-    
+
     //update innerText with pet stats and return
     p.innerText = `${this.age} | ${this.active_status} | ${this.happiness}`
     return p
   }
-  
+
   pImg(){
     //create img element to display pet picture
     const img = document.createElement('img')
-    
+
     //update image source and return
     img.src = this.image
     return img
   }
-  
+
   pBio(){
     //create div to display bio
     const div = document.createElement('div')
-    
+
     //update div innerText with Cat Ipsum gibberish
     div.innerText = "Peer out window, chatter at birds, lure them to mouth sniff sniff the door is opening! how exciting oh, it's you, meh. Sleep on dog bed, force dog to sleep on floor cats go for world domination and human is washing you why halp oh the horror flee scratch hiss bite for i will ruin the couch with my claws crusty butthole so human clearly uses close to one life a night no one naps that long so i revive by standing on chestawaken! mesmerizing birds..."
-    
+
     //return div to render later
     return div
   }
-  
+
   petScore() {
     //create div element to contain happiness info
     const div = document.createElement('div')
-    
+
     //update innerText and add class for .css changes
     div.innerText = this.happiness
     div.id = "score"
-    
+
     return div
   }
 
@@ -127,35 +127,35 @@ class Pet {
     petDiv.append(div)
     
   }
-  
+
   controlPanel() {
     //create buttons
     const ctrlPanel = document.querySelector('#control-panel')
     const feedBtn = document.createElement('button')
     const thirstBtn = document.createElement('button')
     const sleepBtn = document.createElement('button')
-    
+
     feedBtn.className = 'btn btn-secondary'
     feedBtn.innerText = "Feed"
     thirstBtn.className = 'btn btn-secondary'
     thirstBtn.innerText = "Give Water"
     sleepBtn.className = 'btn btn-secondary'
     sleepBtn.innerText = "Sleep"
-    
-    
+
+
     ctrlPanel.append(feedBtn, thirstBtn, sleepBtn)
-    
+
   }
-  
+
   render() {
     //append pet image to the DOM and populate the pet container with the pet
     document.querySelector('#pet-picture').append(this.pImg())
     this.petDiv()
     this.controlPanel()
   }
-  
+
   showSkeletonBody(){
-    document.querySelector("#main-container").innerHTML = 
+    document.querySelector("#main-container").innerHTML =
       `
    <div class="tile is-ancestor">
         <div class="tile is-vertical is-8">
@@ -184,7 +184,7 @@ class Pet {
               <div class="tile is-child box is-success" id="stats">
                   <p class="title">Pet Stats</p>
                   <div id="pet-stats">Age: 2 | Breed: Dog | Something Else</div>
-                  
+
                   <p class="subtitle">Bio:</p>
                   <p>
                     <!-- Content -->
@@ -194,7 +194,7 @@ class Pet {
                   <div class="tile is-child box" id="happiness">
                   <p class="title">Happiness</p>
                   <p class="subtitle" id="level">Level: </p>
-  
+
                     <!-- Content -->
                     <article class="tile is-child notification box">
                       <div id="happiness-div" class="is-4by3">
@@ -208,18 +208,18 @@ class Pet {
 
     this.updateDOM()
   }
-  
+
   updateDOM(){
     document.querySelector("#pet-pic").src = this.image
-    
+
     //create p element to display pet stats
     const p = document.createElement('p')
-    
+
     //update innerText with pet stats and return
     p.innerText = `${this.age} | ${this.active_status} | ${this.happiness}`
-    
+
     document.querySelector("#pet-stats").append(p)
-    
+
     //update control panel
     const ctrlPanel = document.querySelector("#control-panel")
 //    debugger
@@ -227,20 +227,15 @@ class Pet {
       const a = document.createElement("a")
         a.classList = "button is-primary"
         a.innerText = characteristic.action
-      
+
         ctrlPanel.append(a)
     })
 
 //    debugger
   }
-  
+
 }
 
-<<<<<<< HEAD
 
 
 Pet.all = []
-
-=======
-Pet.all = []
->>>>>>> new_pet_ui
--- src/PetController.js
+++ src/PetController.js
@@ -23,22 +23,48 @@ class PetController{
 
 
   static renderPetForm() {
-    document.querySelector('#main-container').innerHTML = `<form id="pet-form">
-                                                      <h1>Create New Pet</h1>
-                                                      <label>Name</label>
-                                                      <p>
-                                                      <input type="text" value=""/>
-                                                      </p>
-                                                      <label>Age</label>
-                                                      <p>
-                                                      <input type="text" value=""/>
-                                                      </p>
-                                                      <label>Image</label>
-                                                      <p>
-                                                      <input type="text" value=""/>
-                                                      </p>
-                                                      <button type="submit">Submit Pet</button>
-                                                    </form>`
+    document.querySelector('#main-container').innerHTML = `
+                                                      <div class="container" id='pet-form-container'>
+                                                      <div class="tile is-parent">
+                                                      <article class="tile is-child notification is-warning">
+                                                      <form id="pet-form">
+                                                      <h1 class="title" id="create-pet ">Create New Pet</h1>
+                                                      <div class="field">
+                                                      <label class="label is-large">Name</label>
+                                                      <div class="control">
+                                                      <input class="input is-large" type="text" placeholder="Enter Pet Name..."/>
+                                                      </div>
+                                                      </div>
+                                                      <div class="field">
+                                                      <label class="label is-large">Age</label>
+                                                      <div class="control">
+                                                      <input class="input is-large" type="text" placeholder="Enter Pet Age..."/>
+                                                      </div>
+                                                      </div>
+                                                      <div class="field">
+                                                      <label class="label is-large">Image</label>
+                                                      <div class="control">
+                                                      <input class="input is-large" type="text" placeholder="Enter Image Url..."/>
+                                                      </div>
+                                                      </div>
+                                                      <div class="field">
+                                                      <label class="label is-large">Bio</label>
+                                                      <div class="control">
+                                                      <textarea class="input is-large" type="text" placeholder="Enter Pet Bio..."></textarea>
+                                                      </div>
+                                                      </div>
+                                                      <div class="field">
+                                                      <div class="control">
+                                                        <button class="button is-danger" type="submit">
+                                                          Submit Pet
+                                                        </button>
+                                                      </div>
+                                                      </div>
+                                                    </form>
+                                                    </article>
+                                                    </div>
+                                                    </div>
+                                                    `
   }
 
   static attachEventListeners() {
@@ -51,12 +77,10 @@ function handleFormSubmit(event) {
   const body = {
     name: document.querySelectorAll('input')[0].value,
     age: document.querySelectorAll('input')[1].value,
-    image: document.querySelectorAll('input')[2].value
+    image: document.querySelectorAll('input')[2].value,
+    bio: document.querySelector('textarea').value
   }
   Adapter.createPet(body).then(newPet => {
     console.log(newPet)
-    document.querySelectorAll('input')[0].value = ''
-    document.querySelectorAll('input')[1].value = ''
-    document.querySelectorAll('input')[2].value = ''
   })
 }
