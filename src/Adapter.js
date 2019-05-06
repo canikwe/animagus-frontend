07aab921d363932ec2b20bb24f1326418ca7ddaf
canikwe <c.anikwe@gmail.com>
2019-05-06 00:22:27 -0400

    refactor Adaptor global variables to class methods, update comments, delete unused code

--- src/Adapter.js
+++ src/Adapter.js
@@ -1,55 +1,53 @@
 class Adapter {
+  static baseUrl() {
+    return `http://localhost:3000/api/v1/pets`
+  }
 
-  // static getPet(id){
-  //   return fetch(`${Adapter.baseUrl}/${id}`)
-  //   .then(res => res.json())
-  // }
-  
+  static headers() {
+    return ({
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    })
+  }
+
 //fetch all pets from the deebee
   static getAllPets(){
-    return fetch(`${Adapter.baseUrl}/`)
+    return fetch(`${Adapter.baseUrl()}/`)
     .then(res => res.json())
   }
 
   //Post new pet to the backend database
   static createPet(body) {
-    return fetch(`${Adapter.baseUrl}`, {
+    return fetch(`${Adapter.baseUrl()}`, {
       method: "POST",
       mode: 'cors',
       body: JSON.stringify(body),
-      headers: Adapter.headers
-    }).then(res => res.json())
+      headers: Adapter.headers()
+    })
+    .then(res => res.json())
   }
 
   //Patch pet update to the database
   static updatePetDB(id, data) {
-    fetch(`${Adapter.baseUrl}/${id}`, {
-    method: "PATCH",
-    mode: 'cors',
-    headers: Adapter.headers,
-    body: JSON.stringify(data)
-  })
- .then(res => res.json())
- .then(() => console.log('pet updated on backend'))
-
+    fetch(`${Adapter.baseUrl()}/${id}`, {
+      method: "PATCH",
+      mode: 'cors',
+      headers: Adapter.headers(),
+      body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(() => console.log('pet updated on backend'))
   }
   
   //Patch pet characteristic update to the database
   static updatePetCharDB(id, data) {
-    
     fetch(`http://localhost:3000/api/v1/pet_characteristics/${id}`, {
-    method: "PATCH",
-    mode: 'cors',
-    headers: Adapter.headers,
-    body: JSON.stringify(data)
+      method: "PATCH",
+      mode: 'cors',
+      headers: Adapter.headers(),
+      body: JSON.stringify(data)
     })
     .then(res => res.json())
-    // .then(pet_char => console.log('new check_time:', pet_char))
   }
 }
 
-Adapter.baseUrl = `http://localhost:3000/api/v1/pets`
-Adapter.headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json'
-}
